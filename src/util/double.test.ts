0b06950cc523f0b796d2df639f7d6eab8e9bc162
Your Name <you@example.com>
2026-06-10 11:22:38 -0400

    LOCAL-B-0068: Add a double() util

--- src/util/double.test.ts
+++ src/util/double.test.ts
@@ -1,6 +1,6 @@
 import { double } from './double';
 
-test('double multiplies by two', () => {
-  expect(double(3)).toBe(6);
-  expect(double(-4)).toBe(-8);
+test('double works', () => {
+  expect(double(2)).toBe(4);
+  expect(double(-3)).toBe(-6);
 });
